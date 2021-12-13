ad04284e3a5b40ad25d007b42edd7f3bef2d7412
manvendraaa <manvendra.rathore1607@gmail.com>
2021-12-13 23:36:48 +0530

    setup express router

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>this is home page</h1>");
-});
+// use express router
+app.use("/", require("./routes/index"));
 
 app.listen(port, (err) => {
   if (err) console.log(`error in running the server ${err}`);
