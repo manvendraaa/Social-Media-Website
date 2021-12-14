5201a62b4c38ce09fea49044ea781e241e18940e
manvendraaa <manvendra.rathore1607@gmail.com>
2021-12-14 11:55:10 +0530

    added sign in sign up forms

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,20 @@ module.exports.profile = (req, res) => {
     title: "profile",
   });
 };
+
+module.exports.signIn = (req,res) => {
+  return res.render('user_sign_in');
+}
+
+module.exports.signUp = (req,res) => {
+  return res.render('user_sign_up');
+}
+
+// sign up form action
+module.exports.create = (req,res)=>{
+  // todo later
+}
+// sign in form action
+module.exports.createSession = (req,res)=>{
+  // todo later
+}
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,7 @@ const usersController = require("../controllers/users_controller");
 const router = express.Router();
 
 router.get("/profile", usersController.profile);
+router.get('/sign-in',usersController.signIn);
+router.get('/sign-up',usersController.signUp);
 
 module.exports = router;
