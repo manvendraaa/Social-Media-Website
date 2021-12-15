21afc586b0f90df8ad8500769d0b2aafbc61ec69
manvendraaa <manvendra.rathore1607@gmail.com>
2021-12-16 01:37:23 +0530

    styling done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,37 +1,54 @@
 //collection of all the actions
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = (req, res) => {
-  // this way you can get the posts but if you want to populate use the second function
-  // Post.find({}, (err,posts)=>{
-  //   return res.render("home", {
-  //     title: "Social Media || Home",
-  //     posts: posts
-  //   });
-  // })
-  // populate the user of each post
-  // for nesting populate like in comments below is the implementation
-  Post.find({})
+// module.exports.home = (req, res) => {
+//   // this way you can get the posts but if you want to populate use the second function
+//   // Post.find({}, (err,posts)=>{
+//   //   return res.render("home", {
+//   //     title: "Social Media || Home",
+//   //     posts: posts
+//   //   });
+//   // })
+//   // populate the user of each post
+//   // for nesting populate like in comments below is the implementation
+//   Post.find({})
+//   .populate('user')
+//   .populate({
+//     path: 'comments',
+//     populate: {
+//       path: 'user'
+//     }
+//   })
+//   .exec((err,posts)=>{
+//     // showing all the users
+//     User.find({},(err,users)=>{
+//       return res.render("home", {
+//         title: "Home",
+//         posts: posts,
+//         all_users: users
+//       });
+//     })
+//   })
+// };
+// same function using async await
+module.exports.home = async (req,res)=>{
+  try{
+    let posts = await Post.find({})
   .populate('user')
   .populate({
     path: 'comments',
     populate: {
       path: 'user'
     }
-  })
-  .exec((err,posts)=>{
-    // showing all the users
-
-    User.find({},(err,user)=>{
-      return res.render("home", {
-        title: "Home",
-        posts: posts,
-        all_users: user
-      });
-    })
-
-    
-  })
-
-  
-};
+  });
+  let users = await User.find({});
+  return res.render("home", {
+    title: "Home",
+    posts: posts,
+    all_users: users
+  });
+  }catch(err){
+    console.log('Error', err);
+    return;
+  }
+}
